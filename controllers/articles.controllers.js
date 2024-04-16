73d565a92d6852a0cf9ed63c97a80a19da8380ac
Daniel Williams <autoboots@gmail.com>
2024-04-16 19:53:00 +0100

    refactors patch and update article functions to reduce number of db queries

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -18,8 +18,8 @@ exports.getArticleById = (req, res, next) => {
 exports.patchVotesByArticleId = (req, res, next) => {
     const { article_id } = req.params
     const body = req.body
-    return Promise.all([updateVotesByArticleId(article_id, body), checkArticleExists(article_id)])
-    .then(([ article ]) => {
+    return updateVotesByArticleId(article_id, body)
+    .then((article) => {
         res.status(200).send({ article })
     })
     .catch(next)
